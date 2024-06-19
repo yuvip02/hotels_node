801a4ddc2048d2b3710076581014542c8020b8cb
Yuvaan Pradhan <yuvi.pradhan2002@gmail.com>
2024-06-19 11:48:39 +0530

    trial commit

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.use("/person",personRoutes)
 const menuRoutes=require("./routes/menuRoutes")
 app.use("/menu",menuRoutes)
 
+
+//app starts server
 app.listen(3000,()=>
 {
     console.log("Server listening on port 3000")
